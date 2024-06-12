bc5e275a7fcdde589669030b525f58ff0be5716d
Meendo <meenphilip@gmail.com>
2024-06-12 22:12:23 +0300

    cleaning form fields and override ModelForm save method

--- images/forms.py
+++ images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 
 
 class ImageCreateForm(forms.ModelForm):
@@ -9,3 +12,28 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             "url": forms.HiddenInput,
         }
+
+    # Cleaning form fields
+    def clean_url(self):
+        url = self.cleaned_data["url"]
+        valid_extensions = ["jpg", "jpeg", "png"]
+        extension = url.rsplit(".", 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError(
+                "The given URL does not match valid image extensions."
+            )
+        return url
+
+    # Overriding the save() method of a ModelForm
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data["url"]
+        name = slugify(image.title)
+        extensions = image_url.rsplit(".", 1)[1].lower()
+        image_name = f"{name}.{extensions}"
+        # download image from the given URL
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+        if commit:
+            image.save()
+        return image
