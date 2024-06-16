f6f6ee5b95e873f7311acc8dab3f6cb8350b2a58
Meendo <meenphilip@gmail.com>
2024-06-16 20:36:35 +0300

    bookmarking images from urls

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("account.urls")),
     path("social-auth/", include("social_django.urls", namespace="social")),
+    path("images/", include("images.urls", namespace="images")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
--- images/urls.py
+++ images/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+app_name = "images"
+
+urlpatterns = [
+    path("create/", views.image_create, name="create"),
+]
