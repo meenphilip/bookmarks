48628807b657912a897357f2617a8e9b5e717b1d
Meendo <meenphilip@gmail.com>
2024-07-01 15:08:31 +0300

    Adding user follow/unfollow actions with JavaScript

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -4,15 +4,15 @@ const minWidth = 250;
 const minHeight = 250;
 
 // load CSS
-var head = document.getElementsByTagName("head")[0]; // Get HTML head element
-var link = document.createElement("link"); // Create new link Element
+let head = document.getElementsByTagName("head")[0]; // Get HTML head element
+let link = document.createElement("link"); // Create new link Element
 link.rel = "stylesheet"; // set the attributes for link element
 link.type = "text/css";
 link.href = styleUrl + "?r=" + Math.floor(Math.random() * 9999999999999999);
 head.appendChild(link); // Append link element to HTML head
 
 // load HTML
-var body = document.getElementsByTagName("body")[0];
+let body = document.getElementsByTagName("body")[0];
 boxHtml = `
   <div id="bookmarklet">
     <a href="#" id="close">&times;</a>
@@ -23,7 +23,7 @@ body.innerHTML += boxHtml;
 
 function bookmarkletLaunch() {
   bookmarklet = document.getElementById("bookmarklet");
-  var imagesFound = bookmarklet.querySelector(".images");
+  let imagesFound = bookmarklet.querySelector(".images");
 
   // clear images found
   imagesFound.innerHTML = "";
@@ -41,7 +41,7 @@ function bookmarkletLaunch() {
   );
   images.forEach((image) => {
     if (image.naturalWidth >= minWidth && image.naturalHeight >= minHeight) {
-      var imageFound = document.createElement("img");
+      let imageFound = document.createElement("img");
       imageFound.src = image.src;
       imagesFound.append(imageFound);
     }
