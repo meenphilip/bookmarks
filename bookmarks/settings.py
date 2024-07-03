2df8a70ec8a3970e48922e074637c280b94eabfb
Meendo <meenphilip@gmail.com>
2024-07-03 13:09:11 +0300

    Building a generic activity stream application

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -72,9 +72,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "images.apps.ImagesConfig",
+    "actions.apps.ActionsConfig",
     "social_django",
     "django_extensions",
-    "images.apps.ImagesConfig",
     "easy_thumbnails",
     "debug_toolbar",
 ]
