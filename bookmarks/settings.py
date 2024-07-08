1dcf49a559b52c54759d0755f4f0a15159226a5f
Meendo <meenphilip@gmail.com>
2024-07-08 13:11:10 +0300

    Using Django Debug Toolbar

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -81,6 +81,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -88,8 +89,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # added b4 the book
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 # social Auth apps
